92aa73a98b99dd3a60884c9a81c590cd5d52a0c0
seancrowe <seancrowe.git@gmail.com>
2024-01-05 14:10:28 -0600

    Add example to reporting.js

--- src/reporting.js
+++ src/reporting.js
@@ -1,6 +1,17 @@
+const example = {
+    documentName: "",
+    documentId: "",
+    taskToCompleteTimeFromXml: "",
+    taskToCompleteTimeFromRequest: "",
+    taskFailed: false,
+    missingUrl: true
+}
+
+
 const reporter = {
 
 }
+
 
 export { 
     reporter 
